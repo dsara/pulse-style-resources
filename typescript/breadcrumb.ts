18a82c64b1870b1db24a493232ccb504c10a547c
Dan Sara <Dan Sara>
2017-03-15 13:10:52 -0600

    use IE compatible remove function

--- typescript/breadcrumb.ts
+++ typescript/breadcrumb.ts
@@ -8,13 +8,23 @@ export namespace BreadCrumb {
 
             for (let i = 0; i < breadCrumbs.length; i++) {
                 if (breadCrumbs.item(i).textContent === "Pages" && pageTitle !== "Pages") {
-                    breadCrumbs.item(i).nextElementSibling.remove();
-                    breadCrumbs.item(i).remove();
+                    // breadCrumbs.item(i).nextElementSibling.remove();
+                    // breadCrumbs.item(i).remove();
+                    
+                    // above remove is not supported in IE, so below is a fix for it
+                    var nextSibling = breadCrumbs.item(i).nextElementSibling;
+                    nextSibling.parentNode.removeChild(nextSibling);
+                    breadCrumbs.item(i).parentNode.removeChild(breadCrumbs.item(i));
                 }
 
                 if (breadCrumbs.item(i).textContent === "SitePages" && pageTitle !== "SitePages") {
-                    breadCrumbs.item(i).nextElementSibling.remove();
-                    breadCrumbs.item(i).remove();
+                    // breadCrumbs.item(i).nextElementSibling.remove();
+                    // breadCrumbs.item(i).remove();
+
+                    // above remove is not supported in IE, so below is a fix for it
+                    var nextSibling = breadCrumbs.item(i).nextElementSibling;
+                    nextSibling.parentNode.removeChild(nextSibling);
+                    breadCrumbs.item(i).parentNode.removeChild(breadCrumbs.item(i));
                 }
             }
         }
