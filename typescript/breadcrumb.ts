78f772555ae4470d9f44385e89accf7f8d5e5e1d
Daniel Sara <daniel.sara@hsc.utah.edu>
2017-03-22 17:00:21 -0600

    added logic to handle removing and fixing of spans

--- typescript/breadcrumb.ts
+++ typescript/breadcrumb.ts
@@ -3,6 +3,8 @@ import {Utilities} from "./utilities";
 export namespace BreadCrumb {
     export function setupBreadcrumb() {
         if (Utilities.getCachedElementById("siteBreadcrumb")) {
+
+            // removes 'pages' and 'sitepages' from the breadcrumb unless you are looking at the library view of 'pages' or 'sitepages'
             let breadCrumbs = document.querySelectorAll(".uu-pulse-site-breadcrumb > span").item(1).querySelectorAll("span");
             let pageTitle = (Utilities.getCachedElementById("DeltaPlaceHolderPageTitleInTitleArea") as HTMLSpanElement) ? (Utilities.getCachedElementById("DeltaPlaceHolderPageTitleInTitleArea") as HTMLSpanElement).textContent.trim() : "";
 
@@ -25,6 +27,63 @@ export namespace BreadCrumb {
                     var nextSibling = breadCrumbs.item(i).nextElementSibling;
                     nextSibling.parentNode.removeChild(nextSibling);
                     breadCrumbs.item(i).parentNode.removeChild(breadCrumbs.item(i));
+                }
+            }
+
+            // removes the dashes from the second part of the breadcrumb
+            // this is common on publishing and wiki pages
+            let secBreadcrumbLinks = document.querySelectorAll(".uu-pulse-site-breadcrumb > span[id*='SiteMapPath1'] > span > a");
+
+            for (let i = 0; i < secBreadcrumbLinks.length; i++) {
+                secBreadcrumbLinks.item(i).textContent = secBreadcrumbLinks.item(i).textContent.replace(/\-/g, " ");
+            }
+
+            // variables to iterate through each breadcrumb
+            let firstBreadCrumbSpans = document.querySelectorAll(".uu-pulse-site-breadcrumb > span[id*='SitesMapPath'] > span");
+            let secondBreadCrumbSpans = document.querySelectorAll(".uu-pulse-site-breadcrumb > span[id*='SiteMapPath1'] > span");
+
+            // removes duplicates on second half of the breadcrumb that exist on the second half of the breadcrumb
+            for (let i = 0; i < secondBreadCrumbSpans.length; i++) {
+                let currentHref = (secondBreadCrumbSpans.item(i).querySelector("a") as HTMLAnchorElement).href;
+                if (currentHref) {
+                    for (let j = 0; j < secondBreadCrumbSpans.length; j++) {
+                        if (j > i) {
+                            if ((secondBreadCrumbSpans.item(j).querySelector("a") as HTMLAnchorElement).href === currentHref) {
+                                let secondSibling = secondBreadCrumbSpans.item(j).nextElementSibling;
+                                secondSibling.parentNode.removeChild(secondSibling);
+                                secondBreadCrumbSpans.item(j).parentNode.removeChild(secondBreadCrumbSpans.item(j));
+                            }
+                        }
+                    }
+                }
+            }
+
+            // removes duplicates on second half of the breadcrumb that exist on the first half of the breadcrumb
+            for (let i = 0; i < firstBreadCrumbSpans.length; i++) {
+                let currentHref = (firstBreadCrumbSpans.item(i).querySelector("a") as HTMLAnchorElement).href;
+                if (currentHref) {
+                    for (let j = 0; j < secondBreadCrumbSpans.length; j++) {
+                        if (j > i) {
+                            if ((secondBreadCrumbSpans.item(j).querySelector("a") as HTMLAnchorElement).href === currentHref) {
+                                let secondSibling = secondBreadCrumbSpans.item(j).nextElementSibling;
+                                secondSibling.parentNode.removeChild(secondSibling);
+                                secondBreadCrumbSpans.item(j).parentNode.removeChild(secondBreadCrumbSpans.item(j));
+                            }
+                        }
+                    }
+                }
+            }
+
+            // removes the slash in the second half of the the breadcrumb if it is the only visible items
+            let numVisibleSecondItems = 0;
+            for (let i = 0; i < secondBreadCrumbSpans.length; i++) {
+                if ((secondBreadCrumbSpans.item(i) as HTMLSpanElement).offsetParent != null) {
+                    numVisibleSecondItems++;
+                }
+            }
+            if (numVisibleSecondItems === 1) {
+                for (let i = 0; i < secondBreadCrumbSpans.length; i++) {
+                    secondBreadCrumbSpans.item(i).parentNode.removeChild(secondBreadCrumbSpans.item(i));
                 }
             }
         }
