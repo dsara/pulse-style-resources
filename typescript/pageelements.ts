2051c673eeedebcda28e847372193e6935642356
dsara <dan.steve.sara@gmail.com>
2018-06-14 13:19:56 -0600

    remove startsWith for backwards compatability

--- typescript/pageelements.ts
+++ typescript/pageelements.ts
@@ -5,7 +5,7 @@ export namespace PageElements {
 
     if (allIFrames && allIFrames.length > 0) {
       for (let i = 0; i < allIFrames.length; i++) {
-        if (allIFrames.item(i).allowFullscreen !== true && allIFrames.item(i).src && allIFrames.item(i).src.startsWith('http')) {
+        if (allIFrames.item(i).allowFullscreen !== true && allIFrames.item(i).src && allIFrames.item(i).src.indexOf('http') === 0) {
           allIFrames.item(i).allowFullscreen = true;
           let iFrameClone = allIFrames.item(i).cloneNode(false);
           allIFrames.item(i).parentNode.replaceChild(iFrameClone, allIFrames.item(i));
